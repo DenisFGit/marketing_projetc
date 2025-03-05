4eb37be4f42d9b87e40ee36bd0ed8c88321178d0
denis <denis@gmail.com>
2025-03-05 12:50:09 +0200

    profit, free and trends sections

--- src/js/script.js
+++ src/js/script.js
@@ -46,5 +46,89 @@ tabsParent.addEventListener('click', function (event) {
     }
 });
 
+//slider
+
+const slides = document.querySelectorAll('.review__item');
+let slideIndex = 0;
+
+const prevBtn = document.querySelector('.review__slider-prev');
+const nextBtn = document.querySelector('.review__slider-next');
+
+console.log(slides);
+
+function initSlider() {
+    if (slides.length > 0) {
+        slides.forEach((item) => {
+            item.classList.add('review__item_hidden');
+        });
+        slides[slideIndex].classList.add('review__item_active');
+        slides[slideIndex].classList.remove('review__item_hidden');
+    }
+}
+
+initSlider();
+
+function showSlide(index) {
+
+    if (index >= slides.length) {
+        slideIndex = 0
+    } else if (index <= 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((item) => {
+        item.classList.remove('review__item_active');
+        item.classList.add('review__item_hidden');
+    });
+    slides[slideIndex].classList.add('review__item_active');
+    slides[slideIndex].classList.remove('review__item_hidden');
+}
+
+function prev() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function next() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+prevBtn.addEventListener('click', prev);
+nextBtn.addEventListener('click', next);
+
+
+const profitList = document.querySelector('.profit__list');
+console.log(profitList);
+
+const profitListItem = document.querySelectorAll('.profit__list li');
+console.log(profitListItem);
+
+function initProfit() {
+    profitListItem.forEach(item => {
+        item.style.borderBottom = 'none';
+    });
+    profitListItem[0].style.borderBottom = '2px solid #009387';
+}
+
+initProfit();
+
+profitList.addEventListener('click', function (e) {
+    let target = e.target.closest('li');
+    if (target && target.getAttribute('data-name') == 'company') {
+        profitListItem.forEach((item, i) => {
+            if (target == item) {
+                profitListItem.forEach(item => {
+                    item.style.borderBottom = 'none';
+                });
+                profitListItem[i].style.borderBottom = '2px solid #009387';
+            }
+
+        });
+    }
+});
+
+// profitListItem.forEach(item => {
+//     console.log(item.getAttribute('data-name'));
+// });
 
 
